52841658fa041cabb3c23d2429857d29e742f129
NguyenHoangYen <nguyenhoangyen037@gmail.com>
2025-04-10 11:53:11 +0700

    Xoa tai khoan nguoi dung khi phat hien gian lan

---
+++ Sudoku_Game/Sudoku_Game/RibbonForm2.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace Sudoku_Game
+{
+    partial class frmAdmin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnToFromLogin = new DevExpress.XtraBars.Ribbon.RibbonControl();
+            this.ribbonPage1 = new DevExpress.XtraBars.Ribbon.RibbonPage();
+            this.ribbonPageGroup1 = new DevExpress.XtraBars.Ribbon.RibbonPageGroup();
+            this.ribbonStatusBar = new DevExpress.XtraBars.Ribbon.RibbonStatusBar();
+            this.btnOut = new DevExpress.XtraBars.BarButtonItem();
+            this.btnDelete = new DevExpress.XtraBars.BarButtonItem();
+            this.barButtonItem1 = new DevExpress.XtraBars.BarButtonItem();
+            this.barButtonItem2 = new DevExpress.XtraBars.BarButtonItem();
+            this.ribbonPageGroup2 = new DevExpress.XtraBars.Ribbon.RibbonPageGroup();
+            this.btnDeleteUser = new DevExpress.XtraBars.BarButtonItem();
+            this.dgvLogin = new System.Windows.Forms.DataGridView();
+            this.colUserName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colPass = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colEmail = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colQuyen = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colScore = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.btnToFromLogin)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLogin)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnToFromLogin
+            // 
+            this.btnToFromLogin.ExpandCollapseItem.Id = 0;
+            this.btnToFromLogin.Items.AddRange(new DevExpress.XtraBars.BarItem[] {
+            this.btnToFromLogin.ExpandCollapseItem,
+            this.btnOut,
+            this.btnDelete,
+            this.barButtonItem1,
+            this.barButtonItem2,
+            this.btnDeleteUser});
+            this.btnToFromLogin.Location = new System.Drawing.Point(0, 0);
+            this.btnToFromLogin.MaxItemId = 6;
+            this.btnToFromLogin.Name = "btnToFromLogin";
+            this.btnToFromLogin.Pages.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPage[] {
+            this.ribbonPage1});
+            // 
+            // 
+            // 
+            this.btnToFromLogin.SearchEditItem.AccessibleName = "Search Item";
+            this.btnToFromLogin.SearchEditItem.Alignment = DevExpress.XtraBars.BarItemLinkAlignment.Left;
+            this.btnToFromLogin.SearchEditItem.EditWidth = 150;
+            this.btnToFromLogin.SearchEditItem.Id = -5000;
+            this.btnToFromLogin.SearchEditItem.ImageOptions.AllowGlyphSkinning = DevExpress.Utils.DefaultBoolean.True;
+            this.btnToFromLogin.Size = new System.Drawing.Size(1024, 231);
+            this.btnToFromLogin.StatusBar = this.ribbonStatusBar;
+            // 
+            // ribbonPage1
+            // 
+            this.ribbonPage1.Groups.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPageGroup[] {
+            this.ribbonPageGroup1,
+            this.ribbonPageGroup2});
+            this.ribbonPage1.Name = "ribbonPage1";
+            this.ribbonPage1.Text = "ribbonPage1";
+            // 
+            // ribbonPageGroup1
+            // 
+            this.ribbonPageGroup1.ItemLinks.Add(this.btnOut);
+            this.ribbonPageGroup1.ItemLinks.Add(this.btnDelete);
+            this.ribbonPageGroup1.ItemLinks.Add(this.barButtonItem1);
+            this.ribbonPageGroup1.ItemLinks.Add(this.barButtonItem2);
+            this.ribbonPageGroup1.Name = "ribbonPageGroup1";
+            this.ribbonPageGroup1.Text = "ribbonPageGroup1";
+            // 
+            // ribbonStatusBar
+            // 
+            this.ribbonStatusBar.Location = new System.Drawing.Point(0, 413);
+            this.ribbonStatusBar.Name = "ribbonStatusBar";
+            this.ribbonStatusBar.Ribbon = this.btnToFromLogin;
+            this.ribbonStatusBar.Size = new System.Drawing.Size(1024, 36);
+            // 
+            // btnOut
+            // 
+            this.btnOut.Caption = "Thoát";
+            this.btnOut.Id = 1;
+            this.btnOut.Name = "btnOut";
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Caption = "Xóa";
+            this.btnDelete.Id = 2;
+            this.btnDelete.Name = "btnDelete";
+            // 
+            // barButtonItem1
+            // 
+            this.barButtonItem1.Caption = "Trở về đăng nhập";
+            this.barButtonItem1.Id = 3;
+            this.barButtonItem1.Name = "barButtonItem1";
+            // 
+            // barButtonItem2
+            // 
+            this.barButtonItem2.Caption = "Bảng Xếp Hạng";
+            this.barButtonItem2.Id = 4;
+            this.barButtonItem2.Name = "barButtonItem2";
+            // 
+            // ribbonPageGroup2
+            // 
+            this.ribbonPageGroup2.ItemLinks.Add(this.btnDeleteUser);
+            this.ribbonPageGroup2.Name = "ribbonPageGroup2";
+            this.ribbonPageGroup2.Text = "ribbonPageGroup2";
+            // 
+            // btnDeleteUser
+            // 
+            this.btnDeleteUser.Caption = "Xóa tài khoản";
+            this.btnDeleteUser.Id = 5;
+            this.btnDeleteUser.Name = "btnDeleteUser";
+            this.btnDeleteUser.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.btnDeleteUser_ItemClick);
+            // 
+            // dgvLogin
+            // 
+            this.dgvLogin.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvLogin.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colUserName,
+            this.colPass,
+            this.colEmail,
+            this.colQuyen,
+            this.colScore});
+            this.dgvLogin.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvLogin.Location = new System.Drawing.Point(0, 231);
+            this.dgvLogin.Name = "dgvLogin";
+            this.dgvLogin.RowHeadersWidth = 62;
+            this.dgvLogin.RowTemplate.Height = 28;
+            this.dgvLogin.Size = new System.Drawing.Size(1024, 182);
+            this.dgvLogin.TabIndex = 2;
+            this.dgvLogin.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvLogin_CellContentClick);
+            // 
+            // colUserName
+            // 
+            this.colUserName.HeaderText = "TK";
+            this.colUserName.MinimumWidth = 8;
+            this.colUserName.Name = "colUserName";
+            this.colUserName.Width = 150;
+            // 
+            // colPass
+            // 
+            this.colPass.HeaderText = "MK";
+            this.colPass.MinimumWidth = 8;
+            this.colPass.Name = "colPass";
+            this.colPass.Width = 150;
+            // 
+            // colEmail
+            // 
+            this.colEmail.HeaderText = "Email";
+            this.colEmail.MinimumWidth = 8;
+            this.colEmail.Name = "colEmail";
+            this.colEmail.Width = 150;
+            // 
+            // colQuyen
+            // 
+            this.colQuyen.HeaderText = "Quyen";
+            this.colQuyen.MinimumWidth = 8;
+            this.colQuyen.Name = "colQuyen";
+            this.colQuyen.Width = 150;
+            // 
+            // colScore
+            // 
+            this.colScore.HeaderText = "Điểm";
+            this.colScore.MinimumWidth = 8;
+            this.colScore.Name = "colScore";
+            this.colScore.Width = 150;
+            // 
+            // frmAdmin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1024, 449);
+            this.Controls.Add(this.dgvLogin);
+            this.Controls.Add(this.ribbonStatusBar);
+            this.Controls.Add(this.btnToFromLogin);
+            this.Name = "frmAdmin";
+            this.Ribbon = this.btnToFromLogin;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.StatusBar = this.ribbonStatusBar;
+            this.Text = "Admin";
+            ((System.ComponentModel.ISupportInitialize)(this.btnToFromLogin)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLogin)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraBars.Ribbon.RibbonControl btnToFromLogin;
+        private DevExpress.XtraBars.Ribbon.RibbonPage ribbonPage1;
+        private DevExpress.XtraBars.Ribbon.RibbonPageGroup ribbonPageGroup1;
+        private DevExpress.XtraBars.Ribbon.RibbonStatusBar ribbonStatusBar;
+        private DevExpress.XtraBars.BarButtonItem btnOut;
+        private DevExpress.XtraBars.BarButtonItem btnDelete;
+        private DevExpress.XtraBars.BarButtonItem barButtonItem1;
+        private DevExpress.XtraBars.BarButtonItem barButtonItem2;
+        private DevExpress.XtraBars.BarButtonItem btnDeleteUser;
+        private DevExpress.XtraBars.Ribbon.RibbonPageGroup ribbonPageGroup2;
+        private System.Windows.Forms.DataGridView dgvLogin;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colUserName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colPass;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colEmail;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colQuyen;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colScore;
+    }
+}
